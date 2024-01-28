8e599511b5fa3403a21a5c23e2b96284eb4b3916
luroblesf <rob23047@byui.edu>
2024-01-27 21:49:33 -0600

    Week 3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
-class Program
+namespace ScriptureMemorizer
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        
+        static void Main(string[] args)
+        {
+            Scripture scripture = new Scripture("John", 3, 16,"For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+            Console.Clear();
+            scripture.Display(); 
+            Console.WriteLine();
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("You did it! Excellent Job!");
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
+            }
+        }
     }
 }
